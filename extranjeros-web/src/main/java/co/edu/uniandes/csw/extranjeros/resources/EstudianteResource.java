6af9e0c95ed6cea10ee18f914469d782e107217c
am.quintero12 <am.quintero12@ISIS2603S2-0079.sis.virtual.uniandes.edu.co>
2018-02-13 12:07:44 -0500

    Creación resource3

--- extranjeros-web/src/main/java/co/edu/uniandes/csw/extranjeros/resources/EstudianteResource.java
+++ extranjeros-web/src/main/java/co/edu/uniandes/csw/extranjeros/resources/EstudianteResource.java
@@ -11,6 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,22 +30,30 @@ import javax.ws.rs.Produces;
 @RequestScoped
 public class EstudianteResource {
     
+    @POST
     public EstudianteDetailDTO createEstudiante(EstudianteDetailDTO estudiante) {
         return estudiante;
     }
     
+    @GET
     public List<EstudianteDetailDTO> getEstudiantes() {
         return new ArrayList<>();
     }
     
+    @GET
+    @Path("{id: \\d+}")
     public EstudianteDetailDTO getEstudiante(@PathParam("id") Long id) {
         return null;
     }
     
+    @PUT
+    @Path("{id: \\d+}")
      public EstudianteDetailDTO updateEstudiante(@PathParam("id") Long id, EstudianteDetailDTO estudiante) {
         return estudiante;
     }
      
+     @DELETE
+    @Path("{id: \\d+}")
      public void deleteEstudiante(@PathParam("id") Long id) {
         
     }
