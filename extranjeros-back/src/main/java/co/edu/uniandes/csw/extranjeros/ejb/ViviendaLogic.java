e101813ce531875ed21c95543114a3aeb796b4c5
Juan diego Arango <jd.arango@uniandes.edu.co>
2018-02-27 11:35:57 -0500

    viviendalogic

---
+++ extranjeros-back/src/main/java/co/edu/uniandes/csw/extranjeros/ejb/ViviendaLogic.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.extranjeros.ejb;
+
+import co.edu.uniandes.csw.extranjeros.entities.ViviendaEntity;
+import co.edu.uniandes.csw.extranjeros.persistence.ViviendaPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jd.arango
+ */
+@Stateless
+public class ViviendaLogic {
+ @Inject
+ ViviendaPersistence persistence;
+ 
+ private static final Logger LOGGER = Logger.getLogger(ServicioLogic.class.getName());
+
+   public List<ViviendaEntity> getViviendas() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todas los viviendas");
+        return persistence.findAll();
+    }
+ 
+ public ViviendaEntity getVivienda(Long id){
+     LOGGER.log(Level.INFO, "Inicia proceso de consultar la vivienda con el id dado");
+       
+     return persistence.find(id);
+ }
+ 
+ public ViviendaEntity createViv(ViviendaEntity vivienda){
+      
+     LOGGER.log(Level.INFO, "Inicia proceso de crear la vivienda");
+     
+     return persistence.create(vivienda);
+ }
+ 
+  public ViviendaEntity updateServicio(ViviendaEntity entity) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar una vivienda");
+        return persistence.update(entity);
+    }
+ 
+   public void deleteVivienda(Long id)
+    {
+        LOGGER.log(Level.INFO,"Inicia proceso de eliminar una vivienda con id={0}",id);
+        persistence.delete(id);
+    }
+  
+}
