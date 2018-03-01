46e8447937c59c2866a4e34f746c207fe5231a39
la.ruiz967 <la.ruiz967@ISIS2603S2-0071.sis.virtual.uniandes.edu.co>
2018-02-28 22:08:23 -0500

    Cambiado en el Entity responsableEventoP de String a Estudiante

--- extranjeros-back/src/main/java/co/edu/uniandes/csw/extranjeros/entities/EventoEntity.java
+++ extranjeros-back/src/main/java/co/edu/uniandes/csw/extranjeros/entities/EventoEntity.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -21,7 +22,6 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Entity
 public class EventoEntity extends BaseEntity implements Serializable{
     
-    private String responsableEventoP;
     private String nombreEvento;
     private String tipoEvento;
     @Temporal(TemporalType.TIMESTAMP)
@@ -36,12 +36,15 @@ public class EventoEntity extends BaseEntity implements Serializable{
      @PodamExclude
    @ManyToMany
     private List<EstudianteEntity> estudiantesInvitados;
+     
+    @OneToOne
+    private EstudianteEntity responsableEventoP;
 
-    public String getResponsableEventoP() {
+    public EstudianteEntity getResponsableEventoP() {
         return responsableEventoP;
     }
 
-    public void setResponsableEventoP(String responsableEventoP) {
+    public void setResponsableEventoP(EstudianteEntity responsableEventoP) {
         this.responsableEventoP = responsableEventoP;
     }
 
