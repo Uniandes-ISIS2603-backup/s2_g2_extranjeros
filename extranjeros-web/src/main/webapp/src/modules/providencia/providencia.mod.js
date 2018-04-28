15a519101b04e4e823a3a0170a1b065321f25a28
am.quintero12 <am.quintero12@ISIS2603S2-0079.sis.virtual.uniandes.edu.co>
2018-04-28 18:01:09 -0500

    Issue Avance 2

--- extranjeros-web/src/main/webapp/src/modules/providencia/providencia.mod.js
+++ extranjeros-web/src/main/webapp/src/modules/providencia/providencia.mod.js
@@ -3,29 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function (ng) {
-    
-    var mod = ng.module("providenciaModule", ['ui.router']);
-    
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            
-            var basePath = 'src/modules/providencia/';
-            
-            $urlRouterProvider.otherwise("/providenciaList");
-            
-            $stateProvider.state('providenciaList', {
-                
-                url: '/providencia/list',
-                 views: {
-                    'mainView': {
-                        templateUrl: basePath + 'providencia.list.html',
-                        controller: 'providenciaCtrl',
-                        controllerAs: 'ctrl'
-                    }
-                }
-            });
-        }
-    ]);
-})(window.angular);
 
 
