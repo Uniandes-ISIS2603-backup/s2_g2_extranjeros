6e142f1a60d1c3ca8188b38708e14d80860876db
Juan diego Arango <jd.arango@uniandes.edu.co>
2018-04-28 23:34:12 -0500

    valoracion y vivienda listos

---
+++ extranjeros-web/src/main/webapp/src/modules/valoraciones/valoraciones.update.ctrl.js
@@ -0,0 +1,35 @@
+
+(function (ng) {
+    var mod = ng.module("valoracionModule");
+    mod.constant("valoracionContext", "valoracion");
+    mod.constant("viviendaContext", "api/viviendas");
+    mod.controller('valoracionesUpdateCtrl', ['$scope', '$http', 'valoracionContext', '$state','$rootScope','viviendaContext',
+       
+        function ($scope, $http, valoracionContext, $state, $rootScope, viviendaContext) {
+            $rootScope.edit = true;
+
+            $scope.data = {};
+
+            $scope.selectedItems = [];
+
+            $scope.availableItems = [];
+
+            var idValoracion = $state.params.valoracionId;
+
+            //Consulto la factura a editar.
+            $http.get(viviendaContext+'/'+$state.params.viviendaId+'/'+'valoracionContext'+'valoraciones'+'/'+idValoracion).then(function (response) {
+                var valoracion = response.data;
+                $scope.data.valoracion = valoracion.valoracion;
+                $scope.data.comentario = valoracion.comentario;
+                 });
+
+            $scope.createValoracion = function () {
+                $http.put(viviendaContext+'/'+$state.params.viviendaId+'/'+'valoraciones' + "/" + idValoracion, $scope.data).then(function (response) {
+                    $state.go('valoracionesList', {valoracionId: response.data.id}, {reload: true});
+                });
+            };
+        }
+    ]);
+}
+)(window.angular);
+
