931ad3fd6c69359dd5b5b4f6a1a9402fc4ad001f
Juan diego Arango <jd.arango@uniandes.edu.co>
2018-05-14 15:12:59 -0500

    put revisado vivienda con asociaciones facturas etc

--- extranjeros-web/src/main/webapp/src/modules/viviendas/vivienda.new.ctrl.js
+++ extranjeros-web/src/main/webapp/src/modules/viviendas/vivienda.new.ctrl.js
@@ -1,12 +1,18 @@
 (function (ng) {
     var mod = ng.module("viviendasModule");
    mod.constant("viviendaContext", "api/viviendas"); 
-   mod.controller('viviendaNewCtrl', ['$scope', '$http','$state', '$rootScope', 'viviendaContext',
-        function($scope, $http, $state, $rootScope, viviendaContext) {
+   mod.constant("servicioContext", "api/servicios"); 
+   mod.constant("lugaresDeInteresContext", "api/lugares"); 
+   mod.constant("estudiantesContext", "api/estudiantes"); 
+  
+   mod.controller('viviendaNewCtrl', ['$scope', '$http','$state', '$rootScope', 'viviendaContext','servicioContext',
+       'lugaresDeInteresContext','estudiantesContext',
+        function($scope, $http, $state, $rootScope, viviendaContext,servicioContext,lugaresDeInteresContext,estudiantesContext) {
 
             $rootScope.edit = false;
 
             $scope.data = {};
+            $scope.servicios = $http.get(servicioContext);
             
             $scope.createVivienda = function () {
                 $http.post(viviendaContext, $scope.data).then(function (response) {
