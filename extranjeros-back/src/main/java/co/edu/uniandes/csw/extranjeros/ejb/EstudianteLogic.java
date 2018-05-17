daba2f948101702f3c09aa4ef2ea3757d1ff021c
am.quintero12 <am.quintero12@ISIS2603S2-0079.sis.virtual.uniandes.edu.co>
2018-05-17 18:03:26 -0500

    IMPORTANTE RELACIONES

--- extranjeros-back/src/main/java/co/edu/uniandes/csw/extranjeros/ejb/EstudianteLogic.java
+++ extranjeros-back/src/main/java/co/edu/uniandes/csw/extranjeros/ejb/EstudianteLogic.java
@@ -26,23 +26,23 @@ import javax.inject.Inject;
  *
  * @author am.quintero12
  */
-
 @Stateless
-public class EstudianteLogic  {
-    
+public class EstudianteLogic {
+
     private static final Logger LOGGER = Logger.getLogger(EstudianteLogic.class.getName());
-    
+
     @Inject
     private EstudiantePersistence estudiantePersistence;
-    
+
     @Inject
     private ProvidenciaPersistence providenciaPersistence;
-    
+
     @Inject
     private UniversidadPersistence universidadPersistence;
-    
+
     /**
      * Devuelve todos los estudiantes que hay en la base de datos.
+     *
      * @return Lista de entidades de tipo libro.
      */
     public List<EstudianteEntity> getEstudiantes() {
@@ -51,9 +51,10 @@ public class EstudianteLogic  {
         LOGGER.info("Termina proceso de consultar todos los estudiantes");
         return estudiantes;
     }
-    
-     /**
+
+    /**
      * Busca un libro por ID
+     *
      * @param id El id del estudiante a buscar
      * @return El estudiante encontrado, null si no lo encuentra.
      */
@@ -66,9 +67,10 @@ public class EstudianteLogic  {
         LOGGER.log(Level.INFO, "Termina proceso de consultar estudiante con id={0}", id);
         return estudiante;
     }
-    
-     /**
+
+    /**
      * Eliminar un estudiante por ID
+     *
      * @param id El ID del estudiante a eliminar
      */
     public void deleteEstudiante(Long id) {
@@ -76,112 +78,109 @@ public class EstudianteLogic  {
         estudiantePersistence.delete(id);
         LOGGER.log(Level.INFO, "Termina proceso de borrar estudiante con id={0}", id);
     }
-    
-  
+
     /**
      * Actualiza la información de un estudiante
+     *
      * @param newUser Instancia de EstudianteEntityy con los nuevos datos.
      * @return Instancia de EstudianteEntityy con los datos actualizados.
      * @throws co.edu.uniandes.csw.extranjeros.exceptions.BusinessLogicException
      */
-    public EstudianteEntity updateEstudiante (EstudianteEntity newUser) throws BusinessLogicException{
-        
+    public EstudianteEntity updateEstudiante(EstudianteEntity newUser) throws BusinessLogicException {
+
         LOGGER.info("Inicia el proceso de actualizar un estudiante en la plataforma");
-        
-         if (universidadPersistence.find(newUser.getUniversidad().getId())!=null){
+
+        if (universidadPersistence.find(newUser.getUniversidad().getId()) != null) {
             Long id = newUser.getUniversidad().getId();
             newUser.setUniversidad(universidadPersistence.find(id));
-        }
-        else{
+        } else {
             throw new BusinessLogicException("No existe la universidad seleccionada");
         }
-        
-        if(newUser.getClave().length() < 8 && newUser.getClave().length() > 15){
+
+        if (newUser.getClave().length() < 8 && newUser.getClave().length() > 15) {
             throw new BusinessLogicException("Su contraseña debe tener más de 8 caracteres y menos de 15");
         }
-        
-         if (!newUser.getCorreo().contains("@") || !newUser.getCorreo().contains(".com")){
+
+        if (!newUser.getCorreo().contains("@") || !newUser.getCorreo().contains(".com")) {
             throw new BusinessLogicException("Su correo no es válido.");
         }
-        
+
         String celular = String.valueOf(newUser.getCelular());
         char numeros[] = celular.toCharArray();
-        
-        if (numeros.length != 10){
+
+        if (numeros.length != 10) {
             throw new BusinessLogicException("Ingrese un celular válido para Colombia.");
         }
-        
-        if(!(newUser.getVivienda()!=null))
-        {
+
+        if (!(newUser.getVivienda() != null)) {
             newUser.setEstadoArrendamiento(false);
         }
-        
-        
+
         return estudiantePersistence.update(newUser);
     }
-    
-      //-- CREATE
+
+    //-- CREATE
     /**
      * Se encarga de crear un Estudiante en la base de datos.
+     *
      * @param newUser Objeto de EstudianteEntity con los datos nuevos.
      * @return Objeto de EstudianteEntity con los datos nuevos y su ID.
      * @throws co.edu.uniandes.csw.extranjeros.exceptions.BusinessLogicException
      */
-    public EstudianteEntity createEstudiante (EstudianteEntity newUser) throws BusinessLogicException{
-        
+    public EstudianteEntity createEstudiante(EstudianteEntity newUser) throws BusinessLogicException {
+
         LOGGER.log(Level.INFO, "Inicia el proceso de crear un estudiante en la plataforma.");
-        
-  
-        if (providenciaPersistence.find(newUser.getProvidencia().getId())!=null){
-            Long id = newUser.getProvidencia().getId();
-            newUser.setProvidencia(providenciaPersistence.find(id));
+
+        if (newUser.getProvidencia() != null) {
+            if (newUser.getProvidencia().getId() != null) {
+                if (providenciaPersistence.find(newUser.getProvidencia().getId()) != null) {
+                    Long id = newUser.getProvidencia().getId();
+                    newUser.setProvidencia(providenciaPersistence.find(id));
+                } else {
+                    throw new BusinessLogicException("No existe la providencia seleccionada");
+                }
+            }
         }
-        else{
-            throw new BusinessLogicException("No existe la providencia seleccionada");
-        }
-        
-        if (universidadPersistence.find(newUser.getUniversidad().getId())!=null){
+
+        if (universidadPersistence.find(newUser.getUniversidad().getId()) != null) {
             Long id = newUser.getUniversidad().getId();
             newUser.setUniversidad(universidadPersistence.find(id));
-        }
-        else{
+        } else {
             throw new BusinessLogicException("No existe la universidad seleccionada");
         }
-        
-        if (estudiantePersistence.findByUsuario(newUser.getUsuario()) != null){
+
+        if (estudiantePersistence.findByUsuario(newUser.getUsuario()) != null) {
             throw new BusinessLogicException("Existe un estudiante con el mismo usuario.");
         }
-        
+
         // Verificacion: no existe un arrendatario con la misma cedula.
-        if (estudiantePersistence.findByCedula(newUser.getCedula()) != null){
+        if (estudiantePersistence.findByCedula(newUser.getCedula()) != null) {
             throw new BusinessLogicException("Existe un estudiante con la misma cedula.");
         }
-        
-        if (!newUser.getCorreo().contains("@") || !newUser.getCorreo().contains(".com")){
+
+        if (!newUser.getCorreo().contains("@") || !newUser.getCorreo().contains(".com")) {
             throw new BusinessLogicException("Su correo no es válido.");
         }
-        
-        if (newUser.getEdad() < 18){
+
+        if (newUser.getEdad() < 18) {
             throw new BusinessLogicException("El estudiante no puede ser menor de edad");
         }
 
-        
-        if(newUser.getClave().length() < 8 && newUser.getClave().length() > 15){
+        if (newUser.getClave().length() < 8 && newUser.getClave().length() > 15) {
             throw new BusinessLogicException("Su contraseña debe tener más de 8 caracteres y menos de 15.");
         }
-      
+
         String celular = String.valueOf(newUser.getCelular());
         char[] numeros = celular.toCharArray();
-        
-        if (numeros.length != 10){
+
+        if (numeros.length != 10) {
             throw new BusinessLogicException("Ingrese un celular válido para Colombia.");
         }
-        
-        if(!(newUser.getVivienda()!=null))
-        {
+
+        if (!(newUser.getVivienda() != null)) {
             newUser.setEstadoArrendamiento(false);
         }
         return estudiantePersistence.create(newUser);
     }
-     
+
 }
